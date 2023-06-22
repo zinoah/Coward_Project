5685db8594d428e37980a2819409d3b939437190
Brogod97 <evergreen1375@gmail.com>
2023-06-22 11:37:21 +0900

    fix: ad-modal 백그라운드 클릭 반응 수정, 스타일링 수정

--- coward/src/main/webapp/resources/js/ad-modal.js
+++ coward/src/main/webapp/resources/js/ad-modal.js
@@ -6,3 +6,9 @@ adCloseBtn.addEventListener("click", () => {
   adModal.classList.remove("is-active");
   overlay.classList.remove("is-active");
 });
+
+/** menubar 활성화 상태에서 뒷배경 클릭 시 비활성화 하는 click 이벤트 */
+overlay.addEventListener("click", () => {
+  adModal.classList.remove("is-active");
+  overlay.classList.remove("is-active");
+});
